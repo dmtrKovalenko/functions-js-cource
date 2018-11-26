40da4825d2f03a80be5f7adf6dcd7ce29ce01341
Khocholava Roman <khocholavaroman@gmail.com>
2018-11-26 15:12:21 +0200

    add 2 task and edit other

--- task/task_2.js
+++ task/task_2.js
@@ -1,31 +1,17 @@
-// function makeSpyOn() {
-// 	let count = 1;
-// 	return function() {
-// 		return count++
-// 		}
-// 		spy.calls {
-
-// 		}
-// 		console.log("Вызов: " + count)
-// 	}
-
-// const spy = makeSpyOn() 
-
-// console.log(spy());
-// console.log(spy());
-// //
-// function makeSpyOn(func) {
-//   function spy() {
-//     func();
-//     return spy.calls++;
-//   }
-//   spy.calls = 0;
-//   return spy;
-// }
-//  function func1() {
-//   console.log(1);
-// }
-//  const spy = makeSpyOn(func1);
-// spy();
-// spy();
-// console.log(spy.calls);
+function makeSpyOn(myFunc1) {
+  function spy() {
+    myFunc1();
+    return spy.calls++;
+  }
+  spy.calls = 0;
+  return spy;
+}
+ function myFunc2() {
+		console.log("tet text");
+}
+ const spy = makeSpyOn(myFunc2);
+spy();
+spy();
+spy();
+spy();
+console.log(spy.calls);
---
+++ task/task_5.js
@@ -0,0 +1,9 @@
+// var user = {
+// 	name: "John Smith",
+// 	sayHi: function() {
+// 		alert(this.name)
+// 	},
+// 	timeoutSayHi: function() {
+// 		myTimeout(this.sayHi, 1000);
+// 	}
+ 
