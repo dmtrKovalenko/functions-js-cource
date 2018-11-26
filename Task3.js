87e61ddccefe5803620d5b19aa40fcc9882e18a2
Vladislav Yuschuk <vladislav.yuschuk@nixsolutions.com>
2018-11-26 15:10:21 +0200

    Fix for Task3, add Task4

--- Task3.js
+++ Task3.js
@@ -1,22 +1,17 @@
 function sequence(param1 = 0, param2 = 1) {
-  sequence.value = param1;
+  let value = param1;
   const step = param2;
   return function() {
-    let outputValue = sequence.value;
-    sequence.value = sequence.value + step;
+    let outputValue = value;
+    value = value + step;
     return outputValue;
   };
 }
 
 const generator = sequence(2, 4);
 
+const generator1 = sequence(5, 3);
 console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
-console.log(generator());
+console.log(generator1());
+console.log(generator1());
 console.log(generator());
